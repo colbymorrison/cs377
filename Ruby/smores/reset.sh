2118f8b17b78002e072de16590425598f043c602
Colby Morrison <colbyamorrison@gmail.com>
2019-05-11 13:22:45 -0400

    reset for arch

--- Ruby/smores/reset.sh
+++ Ruby/smores/reset.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 killall ruby
-ruby ~/cs377/Ruby/server.rb &
+ruby ~/school/cs377/Ruby/server.rb &
 # Let server get started
 sleep 1
-ruby ~/cs377/Ruby/smores/init-ts.rb
+ruby ~/school/cs377/Ruby/smores/init-ts.rb
